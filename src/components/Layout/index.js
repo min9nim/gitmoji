dcb91f6b327a2c247472a1a78517e1ed7b2f0fb9
keating <mgsong@madup.com>
2021-12-30 12:41:29 +0900

    :coffin: remove dead code

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,6 @@
-// @flow
 import * as React from 'react'
-
 import { IconDefinitions } from 'src/components/Icon'
 import Header from './Header'
-import Hamburger from './Hamburger'
 import Footer from './Footer'
 
 type Props = { children: React.Node }
@@ -11,7 +8,6 @@ type Props = { children: React.Node }
 const Layout = (props: Props): React.Node => (
   <>
     <IconDefinitions />
-    <Hamburger />
     <Header withHeadline />
     <main className="wrap">{props.children}</main>
     <Footer />
