8cb6871cb1f32bf88693bf84bd68f830108d6292
Keating <min9nim@gmail.com>
2021-12-30 16:07:24 +0900

    :sparkles: 데이터를 외부 깃헙 레포에서 가져옴

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import gitmojis from 'src/data/gitmojis.json'
+// import gitmojis from 'src/data/gitmojis.json'
 import GitmojiList from 'src/components/GitmojiList'
 import SEO from 'src/components/SEO'
+import axios from 'axios'
+import { prop } from 'ramda'
 
-const Home = () => (
-  <>
-    <SEO />
-    <main>
-      <GitmojiList gitmojis={gitmojis.gitmojis} />
-    </main>
-  </>
-)
+const Home = () => {
+  const [list, setList] = React.useState([])
+
+  React.useEffect(() => {
+    axios
+      .get(
+        'https://raw.githubusercontent.com/madup-inc/chapter-frontend/master/template/gitmoji.json?token=ABOJUXFH552LJHCKIXWDKCDB22JNC'
+      )
+      .then(prop('data'))
+      .then((list) => setList(list))
+  }, [])
+  return (
+    <>
+      <SEO />
+      <main>
+        <GitmojiList gitmojis={list} />
+      </main>
+    </>
+  )
+}
 
 export default Home
