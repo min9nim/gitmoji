8520ab15fbba70d171a1e3dd59410b693c5290fa
Keating <min9nim@gmail.com>
2021-12-30 17:35:33 +0900

    ☀️ Don't Repeat Your code

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const DATA_SOURCE_URL =
+  'https://raw.githubusercontent.com/madup-inc/chapter-frontend/master/template/gitmoji.json?token=ABOJUXFH552LJHCKIXWDKCDB22JNC'
+export const DATA_SOURCE_LOCATION =
+  'https://github.com/madup-inc/chapter-frontend/blob/master/template/gitmoji.json'
